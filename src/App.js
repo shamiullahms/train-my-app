30ea52f0c802fc6c1866be24d7d9b22bbe7e5575
Shamiullah Mustafa <Shamiullah.Mustafa@gaf.com>
2021-01-21 10:30:56 -0500

    Stage 1: With items listing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo192.png";
+
+function Header(props) {
+  //console.log(props);
+  return (
+    <header>
+      <h1>{props.name}'s Page Header</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>This is the {props.adj} section of the page</p>
+      <img src={logo} alt="react logo" />
+      <ul style={{ textAlign: "left" }}>
+        {props.books.map((book) => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>End of the page, built in year {props.year}</p>
+    </footer>
+  );
+}
+
+const books = ["Designer", "Bumblebee", "Caramel", "Notebooks"];
+const bookObjects = books.map((book, i) => ({ id: i, title: book }));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/*       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +52,10 @@ function App() {
           Learn React
         </a>
       </header>
+ */}
+      <Header name="Mirza" />
+      <Main adj="middle" books={bookObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
